b9f30819049d83c891546429657730ca596f6e10
Léo HOTZ <cdf.leo.hotz@gmail.com>
2024-06-14 09:11:53 +0200

    Feat: ajout d'un chargement

--- Broadcast_Front_End/src/Edition.jsx
+++ Broadcast_Front_End/src/Edition.jsx
@@ -3,7 +3,7 @@ import PageLayer from "./PageLayer";
 import InputLabel from "./components/InputLabel";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import SnackBar from "./components/SnackBar";
+import Spinner from "./components/Spinner";
 
 export default function Edition() {
   const { link } = useParams();
@@ -11,8 +11,10 @@ export default function Edition() {
   const [content, setContent] = useState("");
   const [language, setLanguage] = useState("");
   const [theme, setTheme] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
+    setIsLoading(true);
     axios
       .post("http://localhost:443/api/dialoguewithllama/url", link)
       .then((response) => {
@@ -24,41 +26,42 @@ export default function Edition() {
       })
       .catch((error) => {
         console.error(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
       });
   }, [link]);
 
-  // function generate(url, body) {
-  //
-  // }
-
   return (
     <PageLayer title="Edition">
-      <div className="py-10">
-        <SnackBar />
-      </div>
-      <div className="flex w-full flex-col gap-5">
-        <InputLabel label="Lien" value={link} disabled={true} />
-        <InputLabel
-          label="Sujet"
-          setValue={(e) => setSubject(e.target.value)}
-          value={subject}
-        />
-        <InputLabel
-          label="Contenu"
-          setValue={(e) => setContent(e.target.value)}
-          value={content}
-        />
-        <InputLabel
-          label="Langue"
-          setValue={(e) => setLanguage(e.target.value)}
-          value={language}
-        />
-        <InputLabel
-          label="Thème"
-          setValue={(e) => setTheme(e.target.value)}
-          value={theme}
-        />
-      </div>
+      <div className="py-10">{/* <SnackBar /> */}</div>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <div className="flex w-full flex-col gap-5">
+          <InputLabel label="Lien" value={link} disabled={true} />
+          <InputLabel
+            label="Sujet"
+            setValue={(e) => setSubject(e.target.value)}
+            value={subject}
+          />
+          <InputLabel
+            label="Contenu"
+            setValue={(e) => setContent(e.target.value)}
+            value={content}
+          />
+          <InputLabel
+            label="Langue"
+            setValue={(e) => setLanguage(e.target.value)}
+            value={language}
+          />
+          <InputLabel
+            label="Thème"
+            setValue={(e) => setTheme(e.target.value)}
+            value={theme}
+          />
+        </div>
+      )}
     </PageLayer>
   );
 }
