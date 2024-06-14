f53662ad9acdfbbd17b7b6b9351dc4e5cd4941cb
Fatih BOZLAK <bozlak.fatih@gmail.com>
2024-06-14 09:48:17 +0200

    - Ajout de la documentation pour l'API "summarize"

--- Broadcast_Back_End/logique/summarize.js
+++ Broadcast_Back_End/logique/summarize.js
@@ -1,5 +1,16 @@
 const axios = require('axios');
 
+/*
+Description:
+La fonction summarize envoie une requête POST à une API locale pour obtenir le résumé d'un article spécifié par une URL.
+
+Paramètres:
+url (String) : L'URL de l'article à résumer.
+
+Retour:
+Une promesse contenant le résumé de l'article
+ */
+
 async function summarize(url) {
     const postData = {
         model: "llama3",
--- Broadcast_Back_End/routes/dialoguewithllama.js
+++ Broadcast_Back_End/routes/dialoguewithllama.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+
+/*
+Description:
+Cette route est la porte d'entrée pour résumer un article.
+Elle permet de tester le contenu de la requête avant de véritablement l'envoyer à l'IA responsable de résumer.
+
+Paramètres:
+L'url de l'article qu'on souhaite résumer.
+
+Retour:
+Retourne un JSON avec l'article résumé dans la ppt data
+ */
 router.post("/summarize", async (req, res) => {
     if (!req.body.url) {
         return res.status(400).send("URL is required");
