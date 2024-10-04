e070a3fb06f36d2151e861e1eba86eb6ff5a93cb
s4ndalhat <winness.it@gmail.com>
2024-10-04 17:25:41 +0200

    fix: pupeeter docker installation

--- API/logique/extractDataFromUrl.js
+++ API/logique/extractDataFromUrl.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 async function extractDataFromUrl(url, xpath) {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({  args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     const page = await browser.newPage();
     await page.setViewport({
         width: 1000,
