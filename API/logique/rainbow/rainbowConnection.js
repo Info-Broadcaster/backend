0720faa13bcc81a630b753a7471656d55c9700c8
ShortLegsFox <mister.dusbin777@gmail.com>
2024-10-04 11:30:08 +0200

    fix: Simplify the sdk options list & maked the message sent working git add .git add .

--- API/logique/rainbow/rainbowConnection.js
+++ API/logique/rainbow/rainbowConnection.js
@@ -1,145 +1,75 @@
-let RainbowSDK = require('rainbow-node-sdk');
-const {DataStoreType} = require("rainbow-node-sdk/lib/config/config");
+const RainbowSDK = require('rainbow-node-sdk');
 
-class Rainbow {
-    constructor(options) {
-        this.sdk = new RainbowSDK({
-            "rainbow": {
-                "host": "sandbox", // Can be "sandbox" (developer platform), "official" or any other hostname when using dedicated AIO
-                "mode": "xmpp" // The event mode used to receive the events. (default : `xmpp`)
+class RainbowInteraction {
+    constructor(email, password, appId, appSecret) {
+        // Rainbow SDK options
+        this.options = {
+            rainbow: {
+                host: "sandbox", // Use "sandbox" or "official" depending on your environment
             },
-            "credentials": {
-                "login": options.email, // Bot's login to the rainbow platform
-                "password": options.password // Bot's password to the rainbow platform
+            credentials: {
+                login: email, // User email
+                password: password, // User password
             },
-            // Application identifier
-            "application": {
-                "appID": options.appId, // The Rainbow Application Identifier
-                "appSecret": options.appSecret, // The Rainbow Application Secret
+            application: {
+                appID: appId, // Application ID
+                appSecret: appSecret, // Application secret
             },
-            // Logs options
-            "logs": {
-                "enableConsoleLogs": false, // Activate logs on the console
-                "enableFileLogs": false, // Activate the logs in a file<br>
-                "color": true, // Activate the ansii color in the log (more humain readable, but need a term console or reader compatible (ex : vim + AnsiEsc module)) <br>
-                "level": "info", // The level of logs. The value can be "info", "debug", "warn", "error"<br>
-                "customLabel": "MyRBProject", // A label inserted in every lines of the logs. It is usefull if you use multiple SDK instances at a same time. It allows to separate logs in console.<br>
-                "file": {
-                    "path": "c:/temp/", // Path to the log file
-                    "customFileName": "R-SDK-Node-MyRBProject", // A label inserted in the name of the log file
-                    "zippedArchive": false // Can activate a zip of file. It needs CPU process, so avoid it.
-                }
+            logs: {
+                enableConsoleLogs: true, // Enable or disable console logs
+                enableFileLogs: false, // Enable or disable file logs
             },
-            "testOutdatedVersion": true, //Parameter to verify at startup if the current SDK Version is the lastest published on npmjs.com.
-            "requestsRate": { // rate limit of the http requests to server.
-                "maxReqByIntervalForRequestRate": 600, // nb requests during the interval.
-                "intervalForRequestRate": 60, // nb of seconds used for the calcul of the rate limit.
-                "timeoutRequestForRequestRate": 600 // nb seconds Request stay in queue before being rejected if queue is full.
+            im: {
+                sendReadReceipt: true, // Automatically send read receipts
             },
-            // IM options
-            "im": {
-                "sendReadReceipt": true, // If it is setted to true (default value), the 'read' receipt is sent automatically to the sender when the message is received so that the sender knows that the message as been read.
-                "messageMaxLength": 1024, // the maximum size of IM messages sent. Note that this value must be under 1024.
-                "sendMessageToConnectedUser": false, // When it is setted to false it forbid to send message to the connected user. This avoid a bot to auto send messages.
-                "conversationsRetrievedFormat": "small", // It allows to set the quantity of datas retrieved when SDK get conversations from server. Value can be "small" of "full"
-                "storeMessages": true, // Define a server side behaviour with the messages sent. When true, the messages are stored, else messages are only available on the fly. They can not be retrieved later.
-                "nbMaxConversations": 15, // parameter to set the maximum number of conversations to keep (defaut value to 15). Old ones are removed from XMPP server. They are not destroyed. The can be activated again with a send to the conversation again.
-                "rateLimitPerHour": 1000, // Set the maximum count of stanza messages of type `message` sent during one hour. The counter is started at startup, and reseted every hour.
-                "messagesDataStore": DataStoreType.StoreTwinSide, // Parameter to override the storeMessages parameter of the SDK to define the behaviour of the storage of the messages (Enum DataStoreType in lib/config/config , default value "DataStoreType.UsestoreMessagesField" so it follows the storeMessages behaviour)<br>
-                // DataStoreType.NoStore Tell the server to NOT store the messages for delay distribution or for history of the bot and the contact.<br>
-                // DataStoreType.NoPermanentStore Tell the server to NOT store the messages for history of the bot and the contact. But being stored temporarily as a normal part of delivery (e.g. if the recipient is offline at the time of sending).<br>
-                // DataStoreType.StoreTwinSide The messages are fully stored.<br>
-                // DataStoreType.UsestoreMessagesField to follow the storeMessages SDK's parameter behaviour.
+        };
 
-                "autoInitialGetBubbles": true, // to allow automatic opening of the bubbles the user is in. Default value is true.
-                "autoInitialBubblePresence": true, // Define if the presence should be sent automatically to bubbles. This allows to receive the messages from the bubbles.
-                "autoInitialBubbleFormat": "small", // to allow modify format of data received at getting the bubbles. Default value is true.
-                "autoInitialBubbleUnsubscribed": true, // to allow get the bubbles when the user is unsubscribed from it. Default value is true.
-                "autoLoadConversations": true, // Define if the existing conversations on server side should be downloaded at startup. On bot with lot of contacts exchange it can slower the startup.
-                "autoLoadContacts": true // Define if the contacts from the network (the roster) should be loaded at startup.
-            },
+        // Initialize Rainbow SDK
+        this.rainbowSDK = new RainbowSDK(this.options);
+        this.isReady = false;
 
-            // Services to start. This allows to start the SDK with restricted number of services, so there are less call to API.
-            // Take care, severals services are linked, so disabling a service can disturb an other one.
-            // By default all the services are started. Events received from server are not filtered.
-            // So this feature is realy risky, and should be used with much more cautions.
-            "servicesToStart": {
-                "bubbles": {
-                    "start_up": true,
-                }, //need services :
-                "telephony": {
-                    "start_up": true,
-                }, //need services : _contacts, _bubbles, _profiles
-                "channels": {
-                    "start_up": true,
-                }, //need services :
-                "admin": {
-                    "start_up": true,
-                }, //need services :
-                "fileServer": {
-                    "start_up": true,
-                }, //need services : _fileStorage
-                "fileStorage": {
-                    "start_up": true,
-                }, //need services : _fileServer, _conversations
-                "calllog": {
-                    "start_up": true,
-                }, //need services :  _contacts, _profiles, _telephony
-                "favorites": {
-                    "start_up": true,
-                } //need services :
-            }
-        });
-        this.sdk.events.on("rainbow_onmessagereceived", (message) => {
-            // Check if the message is not from you
-            if(!message.fromJid.includes(this.sdk.connectedUser.jid_im)) {
-                // Check that the message is from a user and not a bot
-                if( message.type === "groupchat") {
-                    // Reply to the message
-                    let messageSent = this.sdk.im.sendMessageToBubbleJidAnswer("I got it!", message.fromBubbleJid, "FR", null, 'subject', message);
-                }
-            }
+        // Start Rainbow SDK
+        this.start();
+    }
+
+    // Start the SDK and set up event listeners
+    start() {
+        this.rainbowSDK.start();
+
+        // Handle SDK ready event
+        this.rainbowSDK.events.on("rainbow_onready", () => {
+            console.log("Rainbow SDK is ready.");
+            this.isReady = true;
         });
 
-
+        // Handle errors
+        this.rainbowSDK.events.on("rainbow_onerror", (err) => {
+            console.error("Rainbow SDK error:", err);
+        });
     }
 
-    async connect() {
+    // Method to send a message to a bubble by its JID
+    async sendMessageToBubble(bubbleJid, message) {
+        if (!this.isReady) {
+            console.log("SDK not ready yet, waiting...");
+            // Wait for SDK to be ready
+            await new Promise((resolve) => {
+                this.rainbowSDK.events.once("rainbow_onready", resolve);
+            });
+        }
+
         try {
-            await this.sdk.start();
-            console.log("Rainbow connection works !");
-        } catch (error) {
-            console.log("Rainbow connection failed ! ", error);
-            throw error;
+            const result = await this.rainbowSDK.im.sendMessageToBubbleJid(message, bubbleJid);
+            console.log("Message sent:", result);
+        } catch (err) {
+            console.error("Error sending message:", err);
         }
     }
 
-    async getAllBubbles() {
-        try {
-            let bubbles = this.sdk.bubbles.getAll();
-            console.log("Bubbles retrieved !")
-            return bubbles;
-        } catch (error) {
-            console.log("Cannot get bubbles ! ", error);
-        }
-    }
-
-    async sendMessageToBubble(bubbleJid, message, subject = '', mentions = null, urgency = 'std') {
-        try {
-            const content = {
-                type: 'text',
-                message: message
-            };
-
-            let sentMessage = await this.sdk.im.sendMessageToBubbleJid(message, bubbleJid, "FR", null, 'subject', message);
-
-            console.log("Message sent to bubble:", sentMessage);
-            return sentMessage;
-        } catch (error) {
-            console.error("Error sending message to bubble:", error);
-            throw error;
-        }
+    // Stop the SDK
+    stop() {
+        this.rainbowSDK.stop();
     }
 }
 
-module.exports = Rainbow;
+module.exports = RainbowInteraction;
--- API/logique/rainbowInteractions.js
+++ API/logique/rainbowInteractions.js
@@ -1,20 +1,15 @@
 const Rainbow = require("./rainbow/rainbowConnection");
+
+const email = "mister.dusbin777@gmail.com";
+const password = "D6N{Uz[B5[])";
+const appId = "255f2b9080fd11efa6661b0bb9c90370";
+const appSecret = "Tiw4OORLjL0WTjYbmym6Z2o9p0AuPakNGQUM6bAnRyyJQ7muBz27wmBcxVxWuTix";
+
+const rainbowSdk = new Rainbow(email, password, appId, appSecret);
+
+const message = "Hello bubble !";
 const bubbletestjid = "room_bc2b4784a4054e87b5352c990a2d9b3e@muc.sandbox-all-in-one-rbx-prod-1.rainbow.sbg"
 
-const rainbowConnectionOptions = {
-    email: "mister.dusbin777@gmail.com", // To be completed
-    password: "D6N{Uz[B5[])", // To be completed
-    appId: "255f2b9080fd11efa6661b0bb9c90370",
-    appSecret: "Tiw4OORLjL0WTjYbmym6Z2o9p0AuPakNGQUM6bAnRyyJQ7muBz27wmBcxVxWuTix"
-}
-
-const rainbowSdk = new Rainbow(rainbowConnectionOptions);
-
-rainbowSdk.connect()
-    .then(() => {
-        console.log('Rainbow connection established');
-
-        // Get all bubbles (chat rooms)
-        rainbowSdk.sendMessageToBubble(bubbletestjid, "COUCOU")
-            .then(() => console.log("Message sent !"));
-    })
+rainbowSdk.sendMessageToBubble(bubbletestjid, message).then(
+    () => console.log("Message sent !")
+);
