87654dccc19ec80eb70ab083f96ea8f5d9f654c4
Fatih BOZLAK <bozlak.fatih@gmail.com>
2024-10-13 17:13:08 +0200

    feat: implémentation de Readability (suggéré par Léo <3):
      - par contre, le LLM à vraiment du mal quand le texte à résumé est trop long.
      - Une solution de contournement serait qu'à partir un certain nombre de caractère, de le spliter, et d'envoyer des morceaux. Mais est-ce qu'on va jusqu'à la ?...

--- API/logique/extractDataFromUrl.js
+++ API/logique/extractDataFromUrl.js
@@ -1,4 +1,6 @@
 const puppeteer = require('puppeteer');
+const { Readability } = require('@mozilla/readability');
+const { JSDOM } = require('jsdom');
 
 async function extractDataFromUrl(url, xpath) {
     const browser = await puppeteer.launch({ headless: false });
@@ -10,35 +12,14 @@ async function extractDataFromUrl(url, xpath) {
 
     await page.goto(url, { waitUntil: 'networkidle0' });
 
-    const body = await page.evaluate((xpath) => {
-        if (xpath) {
-            const result = document.evaluate(
-                xpath,
-                document,
-                null,
-                XPathResult.FIRST_ORDERED_NODE_TYPE,
-                null
-            ).singleNodeValue;
-            return result ? result.innerText : null;
-        }
-        return document.body.innerText;
-    }, xpath);
-
-    // const body = `Le Premier ministre prononce cette déclaration devant l’Assemblée nationale. Généralement, au même moment, celle-ci est lue à la tribune du Sénat par un autre membre du gouvernement.
-    //
-    //
-    // Veuillez fermer la vidéo flottante pour reprendre la lecture ici.
-    //
-    //     En janvier dernier, lorsque Gabriel Attal a fait ce fameux discours devant les députés, son texte était lu par Bruno Le Maire, aux sénateurs. Ce dernier avait provoqué des gloussements de son public lorsqu’il avait indiqué « être né en 1989 », lisant mot pour mot, ceux du Premier ministre à l’époque.
-    //
-    //     Est-ce que le discours de politique général est obligatoire ?
-    //     Dans la Constitution de la Ve République, ce discours est simplement mentionné et ne rend pas l’exercice « obligatoire ». Mais cette étape s’est systématisée au fil des années et est devenue une « coutume républicaine ».
-    //
-    // Le site vie-publique.fr a recensé les déclarations de politique générale prononcées après la formation d’un nouveau gouvernement depuis celle prononcée par Michel Debré en 1959.`;
+    const html = await page.content();
+    const dom = new JSDOM(html);
+    const reader = new Readability(dom.window.document);
+    const article = reader.parse().textContent;
 
     await browser.close();
 
-    return body.replace(/\s+/g, ' ');
+    return article.replace(/\s+/g, ' ');
 }
 
 module.exports = extractDataFromUrl;
