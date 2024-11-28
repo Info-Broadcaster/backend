44b1b232636d6655d0acc0183f3a84b6ef37e5a4
Léo HOTZ <cdf.leo.hotz@gmail.com>
2024-11-28 15:37:47 +0100

    Feat(Message): Ajout du titre + lien de l'article

--- API/routes/rainbow-send-message-to-bubbles.js
+++ API/routes/rainbow-send-message-to-bubbles.js
@@ -4,18 +4,19 @@ const Rainbow = require('../logique/rainbow/rainbowInteraction');
 
 router.post('/', async (req, res) => {
     const user = req.user;
-    const { bubbles, message } = req.body; // Extract the bubbleJid and message from the request body.
+    const { bubbles, message, title, link } = req.body; // Extract the bubbleJid and message from the request body.
 
-    if (bubbles == null || bubbles?.length === 0 || !message || message === '') {
-        return res.status(400).json({ error: 'bubbles and message are required.' });
+    if (bubbles == null || bubbles?.length === 0 || !message || message === '' || !title || title === '' || !link || link === '') {
+        return res.status(400).json({ error: 'bubbles, message, title and link are required.' });
     }
 
     const rainbowSdk = new Rainbow(user.username, user.password, process.env.APP_ID, process.env.APP_SECRET);
 
     try {
+        const formatedMessage = title + '\n\n' + message + '\n\n' + link;
+
         for (const bubble of bubbles) {
-            await rainbowSdk.sendMessageToBubble(bubble, message);
-            console.log(`Message sent to bubble ${bubble}`);
+            await rainbowSdk.sendMessageToBubble(bubble, formatedMessage);
         }
         return res.status(200).json({ success: true, message: 'Message sent successfully!.' });
     } catch (error) {
