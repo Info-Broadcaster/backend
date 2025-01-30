399d453a77b6a86486cf7d1450d66a5dccaeb6f9
WRKT <winness.rakotozafy@outlook.fr>
2025-01-30 19:23:14 +0100

    feat: add extractData fromUrl test

--- API/logique/extractDataFromUrl.js
+++ API/logique/extractDataFromUrl.js
@@ -3,25 +3,28 @@ const { Readability } = require('@mozilla/readability');
 const { JSDOM } = require('jsdom');
 
 async function extractDataFromUrl(url, xpath) {
-    const browser = await puppeteer.launch({  args: ['--no-sandbox', '--disable-setuid-sandbox'] });
-    const page = await browser.newPage();
-    await page.setViewport({
-        width: 1000,
-        height: 1000,
-    });
+    let browser;
+    try {
+        browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+        const page = await browser.newPage();
+        await page.setViewport({ width: 1000, height: 1000 });
 
-    page.setDefaultTimeout(300000); // 5 minutes
+        page.setDefaultTimeout(300000); // 5 minutes
+        await page.goto(url, { waitUntil: 'networkidle0' });
 
-    await page.goto(url, { waitUntil: 'networkidle0' });
+        const html = await page.content();
+        const dom = new JSDOM(html);
+        const reader = new Readability(dom.window.document);
+        const article = reader.parse().textContent;
 
-    const html = await page.content();
-    const dom = new JSDOM(html);
-    const reader = new Readability(dom.window.document);
-    const article = reader.parse().textContent;
-
-    await browser.close();
-
-    return article.replace(/\s+/g, ' ');
+        return article.replace(/\s+/g, ' ').trim();
+    } catch (error) {
+        throw new Error('Page failed to load');
+    } finally {
+        if (browser) {
+            await browser.close();
+        }
+    }
 }
 
 module.exports = extractDataFromUrl;
